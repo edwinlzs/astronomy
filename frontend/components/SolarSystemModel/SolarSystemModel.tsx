676e2b0a4fce696db2f9d5134a11f10c8c917c3d
Edwin Lim <edwinlzs97@gmail.com>
2021-12-28 03:04:13 +0800

    SolarSystemModel refactoring + added state for selected planet

--- frontend/components/SolarSystemModel/SolarSystemModel.tsx
+++ frontend/components/SolarSystemModel/SolarSystemModel.tsx
@@ -1,60 +1,39 @@
+import { useState } from 'react';
 import styles from './SolarSystemModel.module.scss';
 
 const SolarSystemModel = () => {
+    const [selected, setSelected] = useState<string>('earth');
+    const systemObjects = [
+        'mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune',
+    ]
+
     return (
+
         <div id={styles.container}>
             <div id={styles.solarSystem}>
-                <div id={styles.mercury} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                        </div>
-                    </div>
-                </div>
-                <div id={styles.venus} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                        </div>
-                    </div>
-                </div>
-                <div id={styles.earth} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                        </div>
-                    </div>
-                </div>
-                <div id={styles.mars} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                        </div>
-                    </div>
-                </div>
-                <div id={styles.jupiter} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                        </div>
-                    </div>
-                </div>
-                <div id={styles.saturn} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                            <div className={styles.ring}>
-
+                {
+                    systemObjects.map((object: string) =>
+                        <div
+                        key={object}
+                        id={styles[object]}
+                        className={styles.orbit}
+                        style={selected === object ? {border: '1px solid rgba(255, 255, 255, 0.8)'} : {}}
+                        >
+                            <div className={styles.pos}>
+                                <div className={styles.planet}>
+                                    {
+                                        object === 'saturn'
+                                            ?
+                                            <div className={styles.ring}>
+                                            </div>
+                                            :
+                                            ""
+                                    }
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div id={styles.uranus} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                        </div>
-                    </div>
-                </div>
-                <div id={styles.neptune} className={styles.orbit}>
-                    <div className={styles.pos}>
-                        <div className={styles.planet}>
-                        </div>
-                    </div>
-                </div>
+                    )
+                }
                 <div id={styles.sun}>
                 </div>
             </div>
